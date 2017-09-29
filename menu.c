4a0e3cec25fafdd912265f7d30e30469122e73b7
Michelle <mlhy9778@hotmail.com>
2017-09-29 13:08:26 +0800

    puchase and print receipt works

--- menu.c
+++ menu.c
@@ -7,9 +7,9 @@
 
 #include <stdio.h>
 
-int order(), show_meals(), purchase_meal();
+int order(), show_combo_meals(), show_addon(), purchase_meal();
 
-int main() {	
+int main() {		
 	order();
 	return 0;
 }
@@ -29,12 +29,37 @@ int order() {
 	puts("6. Show daily transaction");
 	puts("7. Exit");
 
+	printf("Please enter order option (1-7): ");
 	scanf("%d", &order_number);
 
+	while (order_number > 7 || order_number < 1) {
+		printf("Please enter a CORRECT order option (1-7): ");
+		scanf("%d", &order_number);		
+	} 
+
+	//prompt user for purchase again until enters exit option (7)
 	switch(order_number) {
 		case 1:
+			show_combo_meals();	
 			purchase_meal();
-			printf("Purchase!\n");
+
+			// a function for adding on meals
+
+			// puts("--------------------------------------------");
+			// printf("Would you like to addon?");
+			// puts("--------------------------------------------");
+			// puts("(1) YES ");
+			// puts("(2) NO ");
+
+			// scanf("%d", &addon_number);
+
+			// if (addon_number == 1) {
+			// 	show_addon();
+			// } else {
+			// 	printf("Have a nice meal! Your purchases are: %s\n", purchases)
+			// }
+
+			printf("Thanks for purchasing!\n");
 			break;
 		case 2:
 			printf("This option allows user to edit meals!\n");
@@ -46,19 +71,16 @@ int order() {
 			printf("This option allows user to delete meals!\n");
 			break;
 		case 5:
-			show_meals();						
+			show_combo_meals();
+			show_addon();							
 			break;		
 		case 6:
 			printf("Daily transactions!\n");
 			break;
-			// Total combo meal transaction
-			// Total ala-carte transaction
-			// Total sales
-			// GST collected
-			// : 134
-			// : 50
-			// : RM 34108.97
-			// : RM 2047.60
+			// Total combo meal transaction :
+			// Total ala-carte transaction :
+			// Total sales :
+			// GST collected :
 		case 7:
 			printf("Exit!");
 			break;				
@@ -69,14 +91,74 @@ int order() {
 	return 0;
 }
 
-//create function to purchase a meal
-int purchase_meal() {
-
-
+//function to print_receipt
+void print_receipt(int quantity, char *item, float price, float total) {
+	puts("-----------------------------------------------------");									
+	puts("|Qty|\t|Item|\t\t\t\t|Price|");
+	puts("-----------------------------------------------------");									
+	printf("%d\t%s\t\tRM %.2f\n", quantity, item, price);
+	puts("-----------------------------------------------------");									
+	printf("TOTAL: RM%.2f\n", total);
 }
 
-//create function to display all combo meals available
-int show_meals() {
+//function to purchase a meal
+int purchase_meal() {
+
+	char meal_choice[5];
+	char *item;
+	float price, total;
+	int quantity;
+	
+	printf("What would you like to purchase and how many?\n");
+	scanf("%s %d", meal_choice, &quantity);	
+
+	switch (meal_choice[4]) {
+		case '1': 			// assuming they chose set 1 with code C0001 
+			price = 10.99;
+			item = "Lunch Deal Set A";
+			total = price * quantity;
+
+			print_receipt(quantity, item, price, total);
+
+		    break;
+		case '2':
+			price = 12.99;
+			item = "Lunch Deal Set B";
+			total = price * quantity;
+			printf("%.2f\n", total);	
+		    
+		    break;
+		case '3':
+			price = 6.99;
+			item = "Teatime Saver";
+			total = price * quantity;	
+			printf("%.2f\n", total);
+		    
+		    break;
+		case '4':
+			price = 12.99;
+			item = "Dinner Deal Set A";
+			total = price * quantity;	
+			printf("%.2f\n", total);
+		    
+		    break;
+		case '5':
+			price = 14.99;
+			item = "Dinner Deal Set B";
+			total = price * quantity;
+			printf("%.2f\n", total);	
+		    
+		    break;		        
+		default:
+			puts("Invalid meal option!");
+	}
+
+	return 0;
+	
+}
+
+//function to display all combo meals available
+int show_combo_meals() {
 
    FILE *fp;
    char text_file[250];
@@ -84,7 +166,7 @@ int show_meals() {
    fp = fopen("combo.txt", "r"); //opens txt file and READS only
 
    puts("----------------------------------------------");
-   puts("HERE ARE THE COMBO MEALS:");
+   puts(" COMBO MEALS: ");
    puts("----------------------------------------------");
 
    fgets(text_file, 250, (FILE*)fp);
@@ -102,11 +184,53 @@ int show_meals() {
    fgets(text_file, 250, (FILE*)fp);
    printf("%s\n", text_file );
 
+   puts("----------------------------------------------");
+
    fclose(fp); //close the file
+   return 0;
+
+}
+
+int show_addon() {
+
+   FILE *fp;
+   char text_file[250];
+
+   fp = fopen("addon.txt", "r"); //opens txt file and READS only
+
+   puts("----------------------------------------------");
+   puts(" ADD-ONS: ");
+   puts("----------------------------------------------");
+
+   fgets(text_file, 250, (FILE*)fp);
+   printf("%s\n", text_file );
+
+   fgets(text_file, 250, (FILE*)fp);
+   printf("%s\n", text_file );
+   
+   fgets(text_file, 250, (FILE*)fp);
+   printf("%s\n", text_file );
+
+   fgets(text_file, 250, (FILE*)fp);
+   printf("%s\n", text_file );
+
+   fgets(text_file, 250, (FILE*)fp);
+   printf("%s\n", text_file );
+
+   puts("----------------------------------------------");
+
+   fclose(fp); //close txt file
    return 0;
 
 }
 
 // int daily_transactions() {
 	
+// }
+
+// to calculate the total with tax
+// int tax(int price, int quantity) {
+// 	int taxed_total, total;
+// 	taxed_total += (total * 60 / 100);
+// 	return taxed_total;
 // }
